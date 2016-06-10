93d5dc9d7034fc4015046377581e4d7f82fdc81a
Trevor Livingston <tlivings@gmail.com>
2016-06-10 09:21:39 -0500

    initial commit

---
+++ test/test-httprx.js
@@ -0,0 +1,79 @@
+
+const Test = require('tape');
+const RxHttpServer = require('../lib/observables/server');
+const { RxHttpClient } = require('../lib/observables/client');
+const Http = require('http');
+
+Test('test httprx', (t) => {
+
+    t.test('get', (t) => {
+        const server = new RxHttpServer(Http.createServer(), 3000);
+
+        server.subscribe(
+            ({ request, response }) => {
+                response.next('success.');
+                response.complete();
+            },
+            (error) => {
+                console.error(error.stack);
+            },
+            () => {
+                t.end();
+            }
+        );
+
+        new RxHttpClient({method: 'GET', hostname: 'localhost', port: 3000}).subscribe(
+            (response) => {
+                t.equal(response.raw.statusCode, 200, 'OK status.');
+                response.toArray().subscribe(
+                    (data) => {
+                        t.equal(Buffer.concat(data).toString(), 'success.', 'body written.');
+                        server.raw.close();
+                    }
+                );
+            }
+        );
+    });
+
+    t.test('post', (t) => {
+        const server = new RxHttpServer(Http.createServer(), 3000);
+
+        server.flatMap(
+            ({ request, response }) => {
+                return request.toArray().map((body) => {
+                    request.payload = Buffer.concat(body);
+                    return {request, response};
+                });
+            }
+        ).subscribe(
+            ({ request, response }) => {
+                response.next(request.payload);
+                response.complete();
+            },
+            (error) => {
+                console.error(error.stack);
+            },
+            () => {
+                t.end();
+            }
+        );
+
+        const client = new RxHttpClient({method: 'POST', hostname: 'localhost', port: 3000});
+
+        client.subject.next('test');
+        client.subject.complete();
+
+        client.subscribe(
+            (response) => {
+                t.equal(response.raw.statusCode, 200, 'OK status.');
+                response.toArray().subscribe(
+                    (data) => {
+                        t.equal(Buffer.concat(data).toString(), 'test', 'body written.');
+                        server.raw.close();
+                    }
+                );
+            }
+        );
+    });
+
+});
